a82b1cc1feef414fda11f0a0a20c4ea9f54edfa1
zhouhero <zhouhero@hotmail.com>
2017-08-17 15:13:30 +0800

    init

---
+++ static/js/6.a67f9299d72375c3b5f5.js
@@ -0,0 +1,2 @@
+webpackJsonp([6],{1007:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={name:"authredirect",created:function(){var e=window.location.search.slice(1);window.opener.location.href=window.location.origin+"/login#"+e,window.close()}}},999:function(e,n,o){var i=o(1)(o(1007),null,null,null,null);e.exports=i.exports}});
+//# sourceMappingURL=6.a67f9299d72375c3b5f5.js.map
